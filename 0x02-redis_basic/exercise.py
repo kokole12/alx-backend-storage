96f4214c3fb06efaf14f125c6243f6729318170f
kokole12 <kokoleismail886@gmail.com>
2023-07-14 14:52:07 +0300

    exercise solution

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,11 +6,11 @@ from typing import Union
 
 
 class Cache:
-    def __init__(self) -> None:
+    def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, float, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid4)
         self._redis.set(key, data)
         return key
