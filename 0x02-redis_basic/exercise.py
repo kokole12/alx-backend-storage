fa4161e2e900b5d153a64b26c1812d4418fb2296
kokole12 <kokoleismail886@gmail.com>
2023-07-14 15:12:03 +0300

    implemented methods to parametize and a get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """creating a cache class"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -14,3 +14,25 @@ class Cache:
         key = str(uuid4)
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """converting data back to format"""
+        val = self._redis.get(key)
+        if fn:
+            value = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """parametizec Cache.get()"""
+        val = self._redis.get(key).decode('utf-8')
+        return val
+
+    def get_int(self, key: str) -> int:
+        """parametizing Cache.get() to format"""
+        val = self._redis.get(key)
+        try:
+            val = int(val.decode('utf-8'))
+        except Exception:
+            val = 0
+        return val
