517d74a03e1cfe152c235654994fc55c87981910
kokole12 <kokoleismail886@gmail.com>
2023-07-17 18:54:13 +0300

    returning the documemts from the datavbse

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""returning all the mongo collections"""
+"""prototype: def list_all(mongo_collection):"""
 import pymongo
 
 
 def list_all(mongo_collection):
+    """returning all the documentations"""
     if not mongo_collection:
         return []
     documents = mongo_collection.find()
